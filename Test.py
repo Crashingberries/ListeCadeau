5f1eecafed3f1b6f05c94427d7dfa139a457962f
Alex Thibeault <alex96.thibeault@gmail.com>
2018-07-14 13:07:54 -0400

    CreationTableProduit
    Ajout de la Table de produit relié à la table de Client.

--- Test.py
+++ Test.py
@@ -1,10 +1,12 @@
+
 import sqlite3
+
 connexionDB = sqlite3.connect('ListeCadeau.db')
 curseur = connexionDB.cursor()
 
 def CreationTableClient():
     commande_sql = """CREATE TABLE Clients (
-    ID INTEGER PRIMARY KEY,
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
     Nom VARCHAR(40),
     Adresse VARCHAR(120),
     DateDeFete DATE);"""
@@ -14,9 +16,30 @@ def CreationTableClient():
     except sqlite3.OperationalError:
         pass
 
+def CreationTableProduit():
+    commande_sql = """CREATE TABLE `Produits` (
+	`EntreeNo`	INTEGER NOT NULL,
+	`ClientID`	INTEGER NOT NULL,
+	`CUP`	INTEGER,
+	`NomProduit`	TEXT,
+	`Prix`	FLOAT,
+	`Achete`	BOOLEAN,
+	PRIMARY KEY(`EntreeNo`),
+	FOREIGN KEY(`ClientID`) REFERENCES `Clients`(`ID`));"""
+    
+    try:
+        curseur.execute(commande_sql)
+    except sqlite3.OperationalError:
+      pass
+
 def AjoutClient( nom,adresse,date):
     commande_sql ="""INSERT INTO Clients(Nom,Adresse,DateDeFete)
     VALUES (?,?,?);"""
     client= (nom,adresse,date)
     curseur.execute(commande_sql,client)
     connexionDB.commit()
+
+#def AjoutProduit(Client):
+
+CreationTableClient()
+CreationTableProduit()
