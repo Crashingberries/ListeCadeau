aea8838342bebc7bc938074727550b2983bc700d
Alex Thibeault <alex96.thibeault@gmail.com>
2018-07-14 12:05:54 -0400

    CreationTable et AjoutClient
    Le code crée dorrénavant la table si elle n'existe pas et nous pouvons ajouter un client à l'aide de trois paramètres

--- Test.py
+++ Test.py
@@ -2,13 +2,21 @@ import sqlite3
 connexionDB = sqlite3.connect('ListeCadeau.db')
 curseur = connexionDB.cursor()
 
-commande_sql = """CREATE TABLE Clients (
-ID INTEGER PRIMARY KEY,
-Nom VARCHAR(40),
-Adresse VARCHAR(120),
-DateDeNaissance DATE,
-DateDeFete DATE);"""
+def CreationTableClient():
+    commande_sql = """CREATE TABLE Clients (
+    ID INTEGER PRIMARY KEY,
+    Nom VARCHAR(40),
+    Adresse VARCHAR(120),
+    DateDeFete DATE);"""
 
-try  curseur.execute(commande_sql)
+    try:
+        curseur.execute(commande_sql)
+    except sqlite3.OperationalError:
+        pass
 
-else
+def AjoutClient( nom,adresse,date):
+    commande_sql ="""INSERT INTO Clients(Nom,Adresse,DateDeFete)
+    VALUES (?,?,?);"""
+    client= (nom,adresse,date)
+    curseur.execute(commande_sql,client)
+    connexionDB.commit()
